162601909a3bebf8403d7e9d2e3d2ce3a962c894
Carsten Haubold <carstenhaubold@googlemail.com>
2019-08-13 09:56:38 +0200

    Add test for question of life

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -3,4 +3,12 @@
 
 int main() {
 	libbar::run_me();
+
+	if (libbar::question_of_life() != 42)
+	{
+		std::cout << "The answer to the question of life and everything should be 42, was " << libbar::question_of_life() << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
